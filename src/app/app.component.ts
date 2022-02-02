aca88920e17170526ff67ddd70921d7b91eddf1c
Claudia Andrea Quezada Riquelme <cquezada@tisal.local>
2022-02-02 10:32:03 -0300

    base64img

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,33 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'base64img';
+
+  base64textString: string = "";
+
+  constructor(private sanitizer:DomSanitizer){
+  }
+  
+  handleFileSelect(evt: any){
+      var files = evt.target.files;
+      var file = files[0];
+    
+    if (files && file) {
+        var reader = new FileReader();
+
+        reader.onload =this._handleReaderLoaded.bind(this);
+
+        reader.readAsBinaryString(file);
+    }
+  }
+  
+  _handleReaderLoaded(readerEvt: any) {
+     var binaryString = readerEvt.target.result;
+            this.base64textString= btoa(binaryString);
+            console.log(btoa(binaryString));
+    }
+
+  //Call this method in the image source, it will sanitize it.
+  transform(){
+    return this.sanitizer.bypassSecurityTrustResourceUrl('data:image/png;base64,' + this.base64textString);
+}
 }
